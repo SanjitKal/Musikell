cb953f99e25d8a126a105db4fb62c1e377026ba1
Matthew Chiaravalloti <mattch@seas.upenn.edu>
2017-11-13 19:45:59 -0500

    rough sketch of early type ideas

--- Music.hs
+++ Music.hs
@@ -1,41 +1,56 @@
+module Music where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
 type Note = Char
+data Composition =
+    Harmony ([Note], [Mod])
+    | Melody ([Note], [Mod])
 
-data Sound = Harmony [Note] | Melody [Note]
+-- stuff that doesn't change the characteristics of the sound
+-- just changes how the sound(s) are played together
+data Mod =
+    Tempo Int
+    | Duration Int -- quarter note, half note, whole note, etc
+
+type S = Map String Composition
+
+-- add a composition to the S (currently stored sounds)
+add :: String -> Composition -> S -> S 
+add = undefined
+
+-- remove a composition from the S
+remove :: String -> S -> S
+remove = undefined
+
+-- modify a composition in the S with the argued Mod
+modify :: String -> Mod -> S -> S
+modify = undefined
+
+-- modify a composition
+applyMod :: Mod -> Composition -> Composition
+applyMod (Tempo _) h@(Harmony _) = h
+applyMod t@(Tempo _) (Melody (m, ms)) = Melody (m, t:ms)
+
+-- play the sounds
+play :: Composition -> IO ()
+play (Melody  (ns, ms)) = undefined
+play (Harmony (ns, ms)) = undefined
 
 
-type Mod = Temp :: Int -> () | Depth :: Int -> ()
-
-applyMod :: Mod -> Sound -> Sound
-applyMod (Temp f) (Harmony h) = ...
-applyMod (Temp f) (Meldoy m) = different ...
 
 
-harmony >>= mod
-harmony >>= (Temp f) = harmony 
 
-playMelody = ...
-instance Playable a of
-    play :: a -> (sound)
+-- $ musikell
+-- > m a,b,c 120
+-- > show sounds
+-- { s1: Melody [a, b, c], 120 }
+-- > t s1 100
+-- > t 1000
 
-instance Playable Harmony of
-    play (n:ns) = playAll n:ns
-
-add :: Note | Harmony | Melody
-remove
-
-setTempo :: 
-
-$ musikell
-> m a,b,c 120
-> show sounds
-{ s1: Melody [a, b, c], 120 }
-> t s1 100
-> t 1000
-
-melody :: [Note] -> [Mod] -> Melody
-harmony :: [Note] -> [Mod] -> Harmony
-
-m -> melody 
-n -> note
-h -> harmony
-t -> tempo (given a sound)
+-- m -> melody 
+-- n -> note
+-- h -> harmony
+-- t -> tempo (given a sound)
+-- d -> do whatever
