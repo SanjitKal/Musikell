9c837b8f55d75ac012326f3669d4bddd34500593
Matthew Chiaravalloti <mattch@seas.upenn.edu>
2017-12-03 23:52:48 -0500

    unary operations implemented

--- Music.hs
+++ Music.hs
@@ -33,31 +33,90 @@ instance Playable Chord where
 toMusic :: Composition -> Music Pitch
 toMusic (Melody tempo trans m) = Modify (Tempo tempo) (Modify (Transpose trans) (foldr (\c comp -> toMusicPitch c :+: comp) (Prim (Rest 1)) m))
 
+-- instance Monad Chord where
+--     return = Chord
+
+--     (Chord c) >>= f = Chord $ f c
+
+-- instance Applicative Chord where
+--     pure  = return
+--     (<*>) = ap
+
+-- instance Functor Chord where
+--     fmap = liftM
+
+mapChord :: (Note -> Note) -> Chord -> Chord
+mapChord f (Chord c) = Chord $ fmap f c
+
 -- UNARY COMPOSITION OPERATIONS:
-setTempo :: Composition -> Rational -> Composition
-setTempo c t = undefined
 
-modifyTempo :: Composition -> Rational -> Composition
-modifyTempo c t = undefined
+-- | setTempo t c Returns a new Composition with the same melody and transpose
+--      as the argued Compostion c, with the tempo set to the argued t
+setTempo :: Rational -> Composition -> Composition
+setTempo tempo (Melody _ trans m) = Melody tempo trans m
 
-transpose :: Composition -> Int -> Composition
-transpose c t = undefined
+-- | modifyTempo t c Returns a new Composition with the same melody and
+--      transpose as the argued Composition c, with the tempo adjusted by the
+--      argued amount (positive or negative) t
+modifyTempo :: Rational -> Composition -> Composition
+modifyTempo d (Melody tempo trans m) = Melody (tempo + d) trans m 
 
-setInstrument :: Composition -> InstrumentName -> Composition
-setInstrument c i = undefined
+-- | transpose t c Returns a new Composition with the same melody and tempo as
+--      the argued Composition c, with the transpose set to the argured t
+transpose :: Int -> Composition -> Composition
+transpose trans (Melody tempo _ m) = Melody tempo trans m
 
+-- | setInstrument i c Returns a new Composition with the same melody, tempo,
+--      and transpose as the argued Composition c, with the instrument of all
+--      underlying Notes set to the argument InstrumentName i
+setInstrument :: InstrumentName -> Composition -> Composition
+setInstrument i (Melody tempo trans m) = Melody tempo trans $ setI m where
+    setI = fmap $ mapChord (\(N (pp, _)) -> N (pp, i))
+
+-- | reverse c Returns a new Composition with the same tempo and transpose as
+--      the argued Composition c, with the melody reversed
 reverse :: Composition -> Composition
 reverse (Melody tempo trans m) = Melody tempo trans $ List.reverse m
 
-splitAt :: Composition -> Int -> (Maybe Composition, Maybe Composition)
-splitAt c i = undefined
+-- | splitAt i c Returns a 2-tuple of new Compositions. Each Comoposition has
+--      the same tempo and transpose as the argued Composition c. The melody of
+--      the first Composition consists of the first i chords of the melody of
+--      the argued Composition c. The melody of the second Composition consists
+--      of the remaining chords. If i > length melody, then the second
+--      Compsition will be Nothing. If i <= 0, then both compositions will be
+--      Nothing. If either Composition would result as an empty melody, Nothing
+--      will be returned in its place.
+splitAt :: Int -> Composition -> (Maybe Composition, Maybe Composition)
+splitAt i (Melody tempo trans m) =
+    case (c1, c2) of
+        ([], _)  -> (Nothing, Nothing)
+        (m1, []) -> (justMelody m1, Nothing)
+        (m1, m2) -> (justMelody m1, justMelody m2)
+    where
+        (c1, c2)   = List.splitAt i m
+        justMelody = Just . Melody tempo trans
 
-take :: Composition -> Int -> Composition
-take c i = undefined
+-- | take i c Returns a new Composition with the same tempo and transpose as the
+--      argued Composition c, with the melody consisting of its first i chords.
+--      If i <= 0, then Nothing is returned
+take :: Int -> Composition -> Maybe Composition
+take i (Melody tempo trans m) =
+    if i <= 0
+        then Nothing
+        else Just $ Melody tempo trans $ List.take i m
 
-drop :: Composition -> Int -> Composition
-drop c i = undefined
+-- | drop i c Returns a new Composition with the same tempo and transpose as the
+--      argued Composition c, with the melody consisting of all chords after its
+--      first i chords. If i >= length of the melody, then Nothing is returned
+drop :: Int -> Composition -> Maybe Composition
+drop i (Melody tempo trans m) =
+    if i >= length m
+        then Nothing
+        else Just $ Melody tempo trans $ List.drop i m
 
+-- | collapse c Returns a new Composition with the same tempo and transpose as
+--      the argued Composition c, with the melody consisting of a single chord
+--      which is all other chords in the original melody stacked together.
 collapse :: Composition -> Composition
 collapse (Melody tempo trans m) = Melody tempo trans $ pure $ mconcat m
 
@@ -94,6 +153,8 @@ intersperse2 c1 c2 = undefined
 -- ChordA1, ChordB1, ..., Chordn1, c2, Chord(n+1)1, ..., Chord(2n)1, c2, ...
 intersperse2n :: Composition -> Composition -> Int -> Composition
 intersperse2n c1 c2 n = undefined
+
+-- Arbitrary instances
 
 instance Arbitrary InstrumentName where
     arbitrary = elements instruments where
@@ -144,4 +205,3 @@ instance Arbitrary Composition where
     arbitrary = liftM3 Melody (arbitrary :: Gen Rational) (arbitrary :: Gen Int) (arbitrary :: Gen [Chord])
 
     shrink (Melody tempo trans m) = liftM (Melody tempo trans) $ shrink m
-
