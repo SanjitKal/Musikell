1bfe072956672551b3888664e1e832edb4b0be8d
Sanjit Kalapatapu <sanjitkal@gmail.com>
2017-12-11 12:43:45 -0500

    Cleaned up comments a bit more

--- CompositionMap.hs
+++ CompositionMap.hs
@@ -11,24 +11,3 @@ import qualified Data.Map as Map
 
 type MelodyMap      = IncrMap Melody
 type CompositionMap = IncrMap Composition
-
--- -- | An empty CompositionMap. The CID is initialized to 1.
--- empty :: CompositionMap
--- empty = (1, Map.empty)
-
--- -- | Add the argued Composition to the argued CompositionMap. Return the ID of
--- -- | the added Composition along with the updated CompositionMap.
--- add :: CompositionMap -> Composition -> (Int, CompositionMap)
--- add (k, m) c = (k, (k + 1, Map.insert k c m))
-
--- -- | Update an existing Composition with the argued CID  in the argued 
--- -- | CompositionMap by applying an argued function to it.
--- updateWith :: Int -> (Composition -> Composition) -> CompositionMap -> Maybe CompositionMap
--- updateWith cid f (k, m) =
---     case Map.lookup cid m of
---         Just c -> Just (k, Map.insert cid (f c) m)
---         _      -> Nothing
-
--- -- | Return the Composition from the argued CompositionMap with the argued CID
--- get :: CompositionMap -> Int -> Maybe Composition
--- get (k, m) cid = Map.lookup cid m
